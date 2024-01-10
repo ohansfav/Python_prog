396cefbf03faa14782d2d2e021d37ef8ccbf7c10
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-01-10 17:58:37 +0100

    Add files via upload

---
+++ uptesting.py
@@ -0,0 +1,25 @@
+score= 0
+Question_with_answer = [["How many contient does the world contain init?", ["7", "seven"]],["which is the longest river in the world?",["mississippi"]],["which country won the 2006 world cup",["italy"]]]
+
+User_Answered =[[],[]]
+
+
+
+def ask_question(question):
+	answer = input(f"{question[0].upper()}: ").lower().strip()
+	return answer in question[1]
+	
+	
+while True:
+	user_choice = input(f"\nchoose a question from ranging from 1 - {len(Question_with_answer)}".upper())	
+	try:
+		user_choice = int(user_choice.strip()) - 1
+	except ValueError:
+		continue
+	if ask_question(Question_with_answer[user_choice]):
+		score +=2
+		New_question = Question_with_answer.pop()
+		User_Answered.append(New_question)
+	else:
+		score -= 2
+		New_question = Question_with_answer
